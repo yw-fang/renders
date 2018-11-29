4a5191f19b5d67a7948257da74a466c2843d55f2
yw-fang <fyuewen@gmail.com>
2018-11-29 18:15:49 +0900

    change texture and background

--- vapory/examples/multi_spheres.py
+++ vapory/examples/multi_spheres.py
@@ -18,23 +18,34 @@ sphere_2 = Sphere( [0,0,2], 0.3, Texture( Pigment( 'color', [1,0,1.0] )))
 sphere_3 = Sphere( [0,1,0], 0.3, Texture( Pigment( 'color', [1,0,1.0] )))
 sphere_4 = Sphere( [0,0,0], 0.3, Texture( Pigment( 'color', [1,0,1.0] )))
 sphere_5 = Sphere( [1,1,0], 0.3, Texture( Pigment( 'color', [1,0,0.5] )))
-sphere_6 = Sphere( [1,0,0], 0.3, Texture( Pigment( 'color', [1,0,0.5] )))
+# sphere_6 = Sphere( [1,0,0], 0.3, Texture( Pigment( 'color', [1,0,0.5] )))
 sphere_7 = Sphere( [1,0,2], 0.3, Texture( Pigment( 'color', [1,0,0.5] )))
 # sphere_8 = Sphere( [1,1,2], 0.3, Texture( Pigment( 'color', [1,0,0.5] )))
 sphere_6 = Sphere( [1,0,0], 0.3,
-        Texture('T_Glass3', Pigment( 'color', [1.0,0.0,0.5]), Finish('brilliance', 0.45)),
-        Interior('I_Glass4', 'ior', 1.55)
+        Texture(
+            'T_Glass3',
+            Pigment( 'color', [1.0,0.0,0.5]),
+            Finish('phong', 0.45, 'reflection', 0.25, 'specular', 0.500)
+            ),
+        Interior(
+            'I_Glass3', 'ior', 1.55
+            )
         )
 sphere_8 = Sphere( [1,1,2], 0.3,
-        Texture('T_Glass3', Pigment( 'color', [1.0,0.0,0.5]), Finish('brilliance', 0.45)),
-        Interior('I_Glass4', 'ior', 1.55)
+        Texture(
+            'T_Glass3',
+            Pigment( 'color', [1.0,0.0,0.5]),
+            Finish('phong', 0.45, 'reflection', 0.25, 'specular', 0.500)
+            ),
+        Interior(
+            'I_Glass3', 'ior', 1.55
+            )
         )
 
-background = Background("color", [1.0, 1.0, 1.0])
+background = Background("color", [0.0, 0.0, 0.0])
 
-# scene = Scene( camera, objects= [light, sphere_1, sphere_2])
 scene = Scene( camera, objects= [light, background, sphere_1, sphere_2, sphere_3, sphere_4,
     sphere_5, sphere_6, sphere_7, sphere_8],
-    included = ["colors.inc", "textures.inc", "glass.inc"],
-    defaults = [Finish( 'ambient', 0.1, 'diffuse', 0.9)] )
+    included = ["colors.inc", "textures.inc", "glass.inc"])
+#    defaults = [Finish( 'ambient', 0.1, 'diffuse', 0.9)] )
 scene.render("crystal.png", width=900, height=900)
